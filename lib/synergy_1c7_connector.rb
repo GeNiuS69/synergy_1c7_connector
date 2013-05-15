504d44c3d888940f574331208cf2b87371b2d178
PitOn <garik.piton@gmail.com>
2013-05-15 12:37:04 +0400

    setting parent taxon instead of id - correct assignment

--- lib/synergy_1c7_connector.rb
+++ lib/synergy_1c7_connector.rb
@@ -293,7 +293,7 @@ module Synergy1c7Connector
             puts "parsing taxons for taxonomy: #{taxon.taxonomy.name}"            
             groups.each do |group|
                 puts "parsing taxon: #{group.css("Наименование").first.text}"
-                new_taxon = Taxon.find_or_create_by_code_1c(:code_1c => group.css("Ид").first.text, :name => group.css("Наименование").first.text, :taxonomy_id => taxon.taxonomy_id) {|t| t.parent = taxon.id }                
+                new_taxon = Taxon.find_or_create_by_code_1c(:code_1c => group.css("Ид").first.text, :name => group.css("Наименование").first.text, :taxonomy_id => taxon.taxonomy_id) {|t| t.parent = taxon }                
                 parse_groups_from_import_xml(group.css("Группы Группа"), new_taxon) if !group.css("Группы Группа").blank?
             end
         end
