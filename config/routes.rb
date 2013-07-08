ff637a226ccd80cc9664f45f2764ae51ed37e0e8
Alexandr Nigomatulin <nigomatulin@gmail.com>
2013-07-08 12:15:32 +0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+    Spree::Core::Engine.routes.prepend do
   namespace :admin do
     resource :one_c7_connector, :only => [:show, :create]
   end
+    end
   match 'admin/1c_discharge_orders/:id' => 'admin/one_c7_connectors#discharge', :as => :discharge
 end
